6103cdaeedd8d53116c4ca46e41370a64ee1157c
Darya <d.v.zorina@yandex.ru>
2023-07-17 21:44:20 +0300

    Решение задачи

---
+++ Practice3/Pr3T3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+
+Console.WriteLine("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1)
+{
+    Console.WriteLine("X > 0, Y > 0");
+}
+else if (num == 2 || num == "II")
+{
+    Console.WriteLine("X > 0, Y < 0");
+}
+else if (num == 3)
+{
+    Console.WriteLine("X < 0, Y < 0");
+}
+else if (num == 4)
+{
+    Console.WriteLine("X < 0, Y > 0");
+}
+else
+{
+    Console.WriteLine("Такой четверти нет");
+}

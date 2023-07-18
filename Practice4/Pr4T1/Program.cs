3c91e662f8bdbb675bd3689c0e098a8eb381b6b0
Darya <d.v.zorina@yandex.ru>
2023-07-18 10:31:14 +0300

    Решение задачи

---
+++ Practice4/Pr4T1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+while(num > 0){
+num = num / 10;
+result = result + 1;
+}
+Console.WriteLine(result);

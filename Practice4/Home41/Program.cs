9652b14710bdeafa703f13eb498bb82b6e6b44ec
Darya <d.v.zorina@yandex.ru>
2023-07-20 21:32:51 +0300

    Решение задачи

---
+++ Practice4/Home41/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральную степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int proizv = 1;
+for (int i = 0; i < b; i++)
+{
+  proizv = proizv*a;
+}
+Console.WriteLine("Ответ: " + proizv);

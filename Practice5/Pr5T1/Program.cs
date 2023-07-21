2d463b2498388cb596c0add7b8b0d876b8d934e3
Darya <d.v.zorina@yandex.ru>
2023-07-21 21:30:38 +0300

    Решение задачи с массивами

---
+++ Practice5/Pr5T1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+int[] array1 = new int[12];
+int sum_pos = 0;
+int sum_neg = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+   array1[i] = new Random().Next(-9,10);
+   Console.Write(array1[i] + " ");
+
+
+if(array1[i]>0)
+{
+    sum_pos+=array1[i];
+}
+else
+{
+    sum_neg+=array1[i];
+    }
+}
+Console.WriteLine ("Сумма положительных чисел:" + sum_pos);
+Console.WriteLine ("Сумма отрицательных чисел:" + sum_neg);

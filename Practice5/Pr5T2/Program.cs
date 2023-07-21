87f7566828218ef64cc829161faf1a59accd580e
Darya <d.v.zorina@yandex.ru>
2023-07-21 21:47:29 +0300

    Решение задачи

---
+++ Practice5/Pr5T2/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] nums = new int[12];
+
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = new Random().Next(-9, 10);
+    Console.Write(nums[i] + " ");
+}
+Console.WriteLine();
+for (int j = 0; j < nums.Length; j++)
+{
+
+    nums[j] = nums[j] * -1;
+
+    Console.Write(nums[j] + " ");
+}

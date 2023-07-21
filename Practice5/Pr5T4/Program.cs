69230f1501ae4909eaa0ea0cfbafa003cddef5c8
Darya <d.v.zorina@yandex.ru>
2023-07-21 22:24:29 +0300

    Решение задачи

---
+++ Practice5/Pr5T4/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = new int[10];
+int t = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 151);
+    Console.Write(numbers[i] + " ");
+}
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j] > 10 && numbers[j] < 100)
+    {
+        t += 1;
+    }
+}
+Console.WriteLine();
+Console.Write($"Общее количество чисел в данном диапозоне {t}");

7c38519eae82344b398cda734182650c65187409
Darya <d.v.zorina@yandex.ru>
2023-07-22 22:18:37 +0300

    Решение задачи

---
+++ Practice5/Home52/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] array = new int[10];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    Console.Write(array[i] + " ");
+}
+for (int j = 0; j < array.Length; j = j+2)
+{
+    sum+=array[j];
+}
+Console.WriteLine ();
+Console.WriteLine ("Сумма равна: " + sum);

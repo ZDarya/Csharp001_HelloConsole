87182d25f2788d0ce9ac6c5b3852f4a1fda01834
Darya <d.v.zorina@yandex.ru>
2023-07-26 21:30:01 +0300

    Решение задачи

---
+++ Practice6/Pr6T1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+void Fill(int[] numbers)
+{
+    int n = numbers.Length;
+    int i = 0;
+    while (i < n)
+    {
+        numbers[i] = new Random().Next(1, 15);
+        i++;
+    }
+}
+
+
+void Print(int[] nums)
+{
+    int n = nums.Length;
+    int i = 0;
+    while (i < n)
+    {
+        Console.Write(nums[i] + " ");
+        i++;
+    }
+}
+
+void Return(int[] nums)
+{
+    int temp = 0;
+    int n = nums.Length;
+    for (int i = 0; i < n / 2; i++)
+    {
+        temp = nums[i];
+        nums[i] = nums[n - 1 - i];
+        nums[n - 1 - i] = temp;
+    }
+}
+
+int[] array = new int[10];
+
+Fill(array);
+Print(array);
+Console.WriteLine();
+Return(array);
+Print(array);

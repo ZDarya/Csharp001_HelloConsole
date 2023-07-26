1cb78edf3a2e7ed84797041ac7de66e44400c8ea
Darya <d.v.zorina@yandex.ru>
2023-07-26 22:38:32 +0300

    Решение задачи

---
+++ Practice6/PrT5/Program.cs
@@ -0,0 +1,43 @@
+﻿Anastasia: // Задача 45: Напишите программу, которая будет создавать копию
+// заданного массива с помощью поэлементного копирования.
+
+void Fill(int[] numbers)
+{
+int n = numbers.Length;
+int i = 0;
+while (i < n)
+{
+numbers[i] = new Random().Next(1, 15);
+i++;
+}
+}
+
+void Print(int[] nums)
+{
+int n = nums.Length;
+int i = 0;
+while (i < n)
+{
+Console.Write(nums[i] + " ");
+i++;
+}
+Console.WriteLine();
+}
+
+int [] Copy( int[] nums){
+int n = nums.Length;
+int i = 0;
+int [] newArray = new int[n];
+while(i < n){
+newArray[i] = nums[i];
+i++;
+}
+return newArray;
+}
+
+int [] array = new int [10];
+
+Fill(array);
+Print(array);
+int[] array1 = Copy(array);
+Print(array1);

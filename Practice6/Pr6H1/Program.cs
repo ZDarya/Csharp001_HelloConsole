5008a6f661df8f09bc498201b042fe7521c7261b
Darya <d.v.zorina@yandex.ru>
2023-07-28 07:40:06 +0300

    Решение задачи

---
+++ Practice6/Pr6H1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите несколько чисел через пробел и нажмите Enter");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+    count++;
+    }
+}
+Console.WriteLine($"Количество элементов > 0: {count}");

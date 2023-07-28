bdaa030b0db439c734ba701b321e8bf795c99022
Darya <d.v.zorina@yandex.ru>
2023-07-28 11:03:49 +0300

    Решение задачи

---
+++ Practice6/Pr6H2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = (b2-b1) / (k1-k2);
+double y = k1 * x + b1;
+if (k1 * x + b1 == k2 * x + b2)
+{
+Console.WriteLine(x);
+Console.WriteLine(y);
+}
+else
+{
+Console.WriteLine("Прямые не пересекаются");
+}
+
+
+ 

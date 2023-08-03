b240c64559493fe6101e2ee30a80bc4e9619dc6e
Darya <d.v.zorina@yandex.ru>
2023-08-03 21:40:04 +0300

    Решение задачи

---
+++ Practice7/Home71/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+double[,] matrix = new double [4, 4];
+Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 100) + rand.NextDouble();;
+        }
+        
+    }
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(matrix[i, j],2) + " "}");
+        }
+        Console.WriteLine();
+    }

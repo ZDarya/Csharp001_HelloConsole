41942408ad3d85005e295057ecc91a89f5803cc0
Darya <d.v.zorina@yandex.ru>
2023-08-05 21:13:35 +0300

    Решение задачи

---
+++ Practice8/Home1/Program.cs
@@ -0,0 +1,46 @@
+﻿//  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] numbers = new int[3, 6];
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        numbers[i, j] = new Random().Next(0, 10);
+    }
+}
+
+void Print(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(numbers[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Print(numbers);
+Console.WriteLine();
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+                {
+                    for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+                    {
+                        if (numbers[i, z] < numbers[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+                        {
+                            int temp = 0;
+                            temp = numbers[i, z];
+                            numbers[i, z] = numbers[i, z + 1];
+                            numbers[i, z + 1] = temp;
+                        }
+                   }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив с упорядоченными значениями");
+            Print(numbers);

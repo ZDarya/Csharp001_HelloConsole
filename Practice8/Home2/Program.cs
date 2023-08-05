65a906d69d84f9ed45557421c34d0db2a7f355d9
Darya <d.v.zorina@yandex.ru>
2023-08-05 21:32:20 +0300

    Решение задачи

---
+++ Practice8/Home2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] numbers = new int[4, 4];
+int minsum = Int32.MaxValue;
+int indexLine = 0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        numbers[i, j] = new Random().Next(0, 10);
+    }
+}
+
+void Print(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(numbers[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Print(numbers);
+Console.WriteLine();
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    int sum = 0;
+   
+    for (int j = 0; j < numbers.GetLength(1); j++)
+
+    {
+
+        sum += numbers[i, j];
+        
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
+    }
+    Console.Write($"{sum + " "}");
+    
+    }
+    Console.WriteLine();
+    Console.WriteLine(minsum);

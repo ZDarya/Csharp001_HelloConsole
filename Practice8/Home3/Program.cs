e7659f37f13a9b76845add3aea89e46f4855eee9
Darya <d.v.zorina@yandex.ru>
2023-08-06 08:38:30 +0300

    Решение задачи

---
+++ Practice8/Home3/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] firstMatrix = new int [,] {{2, 4}, {3, 2}};
+int[,] secondMatrix = new int [,] {{3, 4}, {3, 3}};
+int[,] resultMatrix = new int[2, 2];
+
+
+// for (int i = 0; i < firstMatrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < firstMatrix.GetLength(1); j++)
+//     {
+//         firstMatrix[i, j] = new Random().Next(0, 10);
+//     }
+//    }
+
+//    for (int i = 0; i < secondMatrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < secondMatrix.GetLength(1); j++)
+//     {
+//         secondMatrix[i, j] = new Random().Next(0, 10);
+//     }
+//    }
+
+
+for (int i = 0; i < firstMatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < secondMatrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < secondMatrix.GetLength(0); k++)
+        {
+            resultMatrix[i, j] += firstMatrix[i, k] * secondMatrix[k, j];
+        }
+        // Console.Write(resultMatrix[i, j] + ", ");
+    }
+    // Console.WriteLine();
+}
+
+
+void Print(int[,] firstMatrix)
+{
+    for (int i = 0; i < firstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < firstMatrix.GetLength(1); j++)
+        {
+            Console.Write(firstMatrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Print(firstMatrix);
+Console.WriteLine();
+Print(secondMatrix);
+Console.WriteLine();
+Print(resultMatrix);

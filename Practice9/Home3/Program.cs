2946d9247a29f345b531f9d14c718945d34d72ac
Darya <d.v.zorina@yandex.ru>
2023-08-10 21:46:19 +0300

    Решение задачи

---
+++ Practice9/Home3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+int Ack(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else if (m == 0) return Ack(n - 1, 1);
+    else return Ack(n - 1, Ack (n, m - 1));
+
+}
+
+int ackerman = Ack(n, m);
+Console.Write($"Результат: {ackerman}");
